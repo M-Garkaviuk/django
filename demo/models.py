1426db0cbe2add2e9ba0597c2df737871d0ad580
Mykola Harkaviuk <garknn@gmail.com>
2023-10-20 11:45:25 -0700

    Main page with all articles and article detailed view added

--- demo/models.py
+++ demo/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 
 
 class Topic(models.Model):
@@ -8,11 +10,11 @@ class Topic(models.Model):
     subscribers = models.ManyToManyField(User, through="Notification")
 
     def __str__(self):
-        return self.title
+        return f'ID: {self.pk}, {self.title}'
 
 
 class Notification(models.Model):
-    subscriber = models.ForeignKey(User, on_delete=models.CASCADE)
+    subscriber = models.ForeignKey(User, on_delete=models.CASCADE, related_name="subscriber")
     topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
     is_notified = models.BooleanField(default=False)
 
@@ -22,18 +24,22 @@ class Article(models.Model):
     content = models.TextField(default='')
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None)
-    topics = models.ManyToManyField(Topic)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name="articles")
+    topics = models.ManyToManyField(Topic, related_name="topics")
 
     def __str__(self):
-        return self.title
+        return f"ID = {self.pk}, subject - {self.title}"
 
 
 class Comment(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     message = models.CharField(max_length=512)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING)
+    article = models.ForeignKey(Article, on_delete=models.DO_NOTHING, related_name="article")
 
     def __str__(self):
         return self.message
+
+    # def get_absolute_url(self):
+    #     return reverse('main:article_detail',
+    #                    args=[str(self.pk)])
--- demo/views.py
+++ demo/views.py
@@ -1,10 +1,13 @@
 # demo.views
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from datetime import datetime
+from django.utils import timezone
+from demo.models import Article
 
 
 def homepage_view(request):
-    return render(request, "index.html", {"page_name": "Home"})
+    return render(request, "main_page.html", {"article": "articles"})
 
 
 def about_view(request):
@@ -42,6 +45,15 @@ def topic_subscribe_view(request, topic: str):
 def topic_unsubscribe_view(request, topic: str):
     return HttpResponse(f'This page is to confirm unsubscription from the topic {topic}')
 
+
+def article_list(request):
+    articles = Article.objects.filter(created__lte=timezone.now()).order_by('created')
+    return render(request, 'main_page.html', {"articles": articles})
+
+
+def article_detail(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    return render(request, "article_detail.html", {"article": article})
 
 
 
